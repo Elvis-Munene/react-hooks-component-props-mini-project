f139b179209d147b739cd0f66d12f249dbdef667
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-07-25 07:24:11 +0300

    Done

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function Header({data}) {
+console.log(data);
+
+    return (
+      <header>
+
+        <h1>{data.name}</h1>
+
+    </header>  
+
+
+
+    )
+
+
+}
+
+export default Header;
+
+
+
